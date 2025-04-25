e6480ba067a5299d070f16e622ea443f12859454
Maxwell Bourcier <6305446+RobotReactor@users.noreply.github.com>
2025-04-25 13:08:52 -0400

    feat: implement MainWindowViewModel with Title property for binding

---
+++ src/WpfWebcamImageProcessor/WpfWebcamImageProcessor.App/ViewModels/MainWindowViewModel.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Prism.Mvvm;
+
+namespace WpfWebcamImageProcessor.App.ViewModels
+{
+    public class MainWindowViewModel : BindableBase 
+    {
+        private string _title = "Webcam Image Processor";
+        public string Title
+        {
+            get { return _title; }
+            set { SetProperty(ref _title, value); }
+        }
+
+        public MainWindowViewModel()
+        {
+            // Constructor - waiting for Emgu.CV to be added before adding more functionality
+        }
+    }
+}
