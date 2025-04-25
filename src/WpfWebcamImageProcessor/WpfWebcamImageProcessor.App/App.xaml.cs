e30dc09405a2ba5e98a0f0f096412000adabd276
Maxwell Bourcier <6305446+RobotReactor@users.noreply.github.com>
2025-04-25 12:57:55 -0400

    feat: configure MainWindow as Prism shell and register for navigation

--- src/WpfWebcamImageProcessor/WpfWebcamImageProcessor.App/App.xaml.cs
+++ src/WpfWebcamImageProcessor/WpfWebcamImageProcessor.App/App.xaml.cs
@@ -1,13 +1,28 @@
-﻿using System.Configuration;
+﻿using Prism.DryIoc;
+using Prism.Ioc;
+using System.Configuration;
 using System.Data;
 using System.Windows;
+using WpfWebcamImageProcessor.App.Views;
 
 namespace WpfWebcamImageProcessor.App;
 
 /// <summary>
 /// Interaction logic for App.xaml
+/// 
+/// Addressing errors in next commit
 /// </summary>
-public partial class App : Application
+public partial class App : PrismApplication
 {
+    protected override void RegisterTypes(IContainerRegistry containerRegistry)
+    {
+        containerRegistry.RegisterForNavigation<MainWindow>(); 
+    }
+
+    protected override Window CreateShell()
+    {
+        return Container.Resolve<MainWindow>();
+    }
+
 }
 
